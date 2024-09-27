6c70fd41a50cdbf013a442cbf8a080bcfb2cd59d
donattodev <130395087+donattodev@users.noreply.github.com>
2024-09-27 11:10:36 -0300

    Estrutura básica do projeto.
    - Instalação do Next
    - Instalação do Shadcn Ui e componentes
    - Instalação das libs:
      - @rocketseat/eslint-config
      - prettier-tailwindcss-plugin

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import '../styles/globals.css'
+import type { Metadata } from 'next'
+import type { ReactNode } from 'react'
+import { Roboto as FontSans } from 'next/font/google'
+import { ThemeProvider } from '@/components/themes/theme-provider'
+
+const roboto = FontSans({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  variable: '--font-roboto',
+})
+
+export const metadata: Metadata = {
+  title: 'Create Next App',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${roboto.variable} font-sans antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
